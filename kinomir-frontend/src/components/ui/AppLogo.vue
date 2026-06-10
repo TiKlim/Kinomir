6ad4fcb441261e3b6407dd54bc5acc982ddf3ae1
TiKlim <karelian_company@mail.ru>
2026-06-10 17:33:15 +0300

    feat: add a layout for the logo

---
+++ kinomir-frontend/src/components/ui/AppLogo.vue
@@ -0,0 +1,26 @@
+<template>
+    <button class="logo-button"
+    @click="$emit('click')">
+    <slot></slot>
+    </button>
+</template>
+
+<script setup>
+defineEmits(['click'])
+</script>
+
+<style scoped>
+.logo-button {
+    background: #11111180;
+    backdrop-filter: blur(4px);
+    font-size: 16px;
+    color: #00B2FF;
+    font-family: 'Unbounded', sans-serif;
+    border: none;
+    border-radius: 12px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    width: 186px;
+    height: 40px;
+}
+</style>
