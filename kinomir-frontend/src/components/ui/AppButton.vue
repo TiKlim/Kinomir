fcd0b9cb77310e1e021e319d61966dd8940e83c5
TiKlim <karelian_company@mail.ru>
2026-06-10 17:32:47 +0300

    feat: add a layout for buttons

---
+++ kinomir-frontend/src/components/ui/AppButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="app-button"
+    @click="$emit('click')">
+<slot></slot>
+</button>
+</template>
+
+<script setup>
+defineEmits(['click'])
+</script>
+
+<style scoped>
+.app-button {
+    background: #54535380;
+    backdrop-filter: blur(4px);
+    color: white;
+    font-size: 16px;
+    border: none;
+    border-radius: 12px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    width: 186px;
+    height: 40px;
+}
+.app-button:hover {
+    background: #6BD2FF80;
+}
+.app-button:active {
+    transform: scale(1.05);
+    background: #00B2FF80;
+    backdrop-filter: blur(4px);
+    color: white;
+}
+</style>
