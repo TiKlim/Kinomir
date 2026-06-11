9036b4a01afee463544df1cef2eb95a30a79a55f
TiKlim <karelian_company@mail.ru>
2026-06-11 13:09:11 +0300

    feat: add DTO for news

---
+++ kinomir-backend/kinomir-backend/DTO/NewsDTO.cs
@@ -0,0 +1,9 @@
+namespace kinomir_backend.DTO
+{
+    public class NewsDTO
+    {
+        public int NewsId { get; set; }
+        public string NewsTitle { get; set; }
+        public string NewsContent { get; set; }
+    }
+}
