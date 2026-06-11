11fe8453bf918548c0222734b1b7e63a6080e4fb
TiKlim <karelian_company@mail.ru>
2026-06-11 13:09:47 +0300

    feat: add DTO for promotions

---
+++ kinomir-backend/kinomir-backend/DTO/PromotionDTO.cs
@@ -0,0 +1,9 @@
+namespace kinomir_backend.DTO
+{
+    public class PromotionDTO
+    {
+        public int PromotionId { get; set; }
+        public string PromotionTitle { get; set; }
+        public string PromotionContent { get; set; }
+    }
+}
